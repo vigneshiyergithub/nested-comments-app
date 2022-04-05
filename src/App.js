c3a0ec844df8a675ac5167661d8611d585e9cf0f
vigneshiyergithub <vigneshiyer94@gmail.com>
2022-04-05 22:39:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const getNewComment = (commentValue, isRootNode = false, parentNodeId) => {
+  return {
+    id: uuidv4(),
+    commentText: commentValue,
+    childCommments: [],
+    isRootNode,
+    parentNodeId,
+  };
+};
+
+const initialState = {};
 
 function App() {
+  const [comments, setComments] = useState(initialState);
+  const [rootComment, setRootComment] = useState("");
+  const addComment = (parentId, newCommentText) => {
+    let newComment = null;
+    if (parentId) {
+      newComment = getNewComment(newCommentText, false, parentId);
+      setComments((comments) => ({
+        ...comments,
+        [parentId]: {
+          ...comments[parentId],
+          childCommments: [...comments[parentId].childCommments, newComment.id],
+        },
+      }));
+    } else {
+      newComment = getNewComment(newCommentText, true, null);
+    }
+    setComments((comments) => ({ ...comments, [newComment.id]: newComment }));
+  };
+  const commentMapper = (comment) => {
+    return {
+      ...comment,
+      childCommments: comment.childCommments
+        .map((id) => comments[id])
+        .map((comment) => commentMapper(comment)),
+    };
+  };
+  const enhancedComments = Object.values(comments)
+    .filter((comment) => {
+      return !comment.parentNodeId;
+    })
+    .map(commentMapper);
+  const onAdd = () => {
+    addComment(null, rootComment);
+    setRootComment("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header style={{marginBottom : '2rem', fontSize : '2rem'}}>Nested Comment Example</header>
+      <div className="comments-container">
+        <input
+          type="text"
+          value={rootComment}
+          onChange={(e) => setRootComment(e.target.value)}
+          placeholder="add comment"
+          style={{ width: "80%", marginRight: "1rem" }}
+        />{" "}
+        <button onClick={onAdd}>Add</button>
+      </div>
+      <div
+        style={{
+          border: "1px solid blue",
+          width: "60%",
+          margin: "auto",
+          overflowX: "auto",
+          padding: "2rem",
+        }}
+      >
+        {enhancedComments.map((comment, key) => {
+          return (
+            <Comment key={key} comment={comment} addComment={addComment} />
+          );
+        })}
+      </div>
     </div>
   );
 }
+
+const Comment = ({ comment, addComment }) => {
+  const { commentText, childCommments, id } = comment;
+  const [childComment, setChildComment] = useState("");
+  const [show, setShow] = useState(true);
+  const [showAddComponet, setShowAddComponet] = useState(false);
+  const onAdd = () => {
+    addComment(id, childComment);
+    setChildComment("");
+    setShowAddComponet(false);
+  };
+  return (
+    <div className="Comment">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{ textAlign: "left" }}>{commentText}</div>
+        &nbsp;
+        {childCommments.length > 0 && (
+          <button onClick={() => setShow((show) => !show)}>
+            {show ? "Hide" : "Show"}
+          </button>
+        )}
+      </div>
+      <div>
+        <div>
+          {showAddComponet ? (
+            <>
+              <input
+                type="text"
+                value={childComment}
+                onChange={(e) => setChildComment(e.target.value)}
+                placeholder="add comment"
+              />{" "}
+              <button onClick={onAdd}>Submit</button>
+            </>
+          ) : (
+            <a
+              style={{ cursor: "pointer", fontSize: "0.7rem", color: "blue" }}
+              onClick={() => setShowAddComponet(true)}
+            >
+              Add a reply
+            </a>
+          )}
+        </div>
+      </div>
+      {show &&
+        childCommments.map((childCommentEl, key) => {
+          return (
+            <Comment
+              key={key}
+              comment={childCommentEl}
+              addComment={addComment}
+            />
+          );
+        })}
+    </div>
+  );
+};
 
 export default App;
